7a3b02112bb6fad016a42f451d84a10fb87a092e
KIMOH3 <hamada111ggu@gmail.com>
2024-05-28 23:21:47 +0300

    Update config.py

--- config.py
+++ config.py
@@ -38,7 +38,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/SOURCE-LORD/TEST.git",
+    "https://github.com/KIMOH3/M-LV.git",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
