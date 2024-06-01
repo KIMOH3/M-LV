777a691fda9052f379f42e1b2b1424fe351708ca
KIMOH3 <hamada111ggu@gmail.com>
2024-06-01 14:51:37 +0300

    Update Dev.py

--- ZelzalMusic/plugins/play/Dev.py
+++ ZelzalMusic/plugins/play/Dev.py
@@ -4,22 +4,20 @@ from strings.filters import command
 from ZelzalMusic import app
 import config
 
-# تعريف الأمر وتحديد الوظيفة المرتبطة به
 @app.on_message(
     command(["المطور", "مطور"])
 )
 async def maker(client: Client, message: Message):
-    # الحصول على معلومات المستخدم
     user = await app.get_users(config.OWNER_ID)
-    
-    # تحديد الصورة المستخدمة، أو استخدام صورة بديلة إذا لم تتوفر صورة
-    photo_url = user.photo.big_file_id if user.photo else "https://telegra.ph/file/dbacb042fe3d9276c687e.jpg"
-    
-    # إنشاء الرد المتكامل مع الصورة والنص والزر الخاص بمطور البوت
+    photo_url = "https://telegra.ph/file/dbacb042fe3d9276c687e.jpg"
+
+    name_caption = f"اسم: {user.first_name}"
+    bio_caption = f"النبذة: {user.bio}"
+
     await app.send_photo(
         chat_id=message.chat.id,
         photo=photo_url,
-        caption="~ 𝐃𝐞𝐯 𝐁𝐨𝐓",
+        caption=f"{name_caption}\n{bio_caption}",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
