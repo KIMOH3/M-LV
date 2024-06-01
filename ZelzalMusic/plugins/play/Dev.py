a676f718d5204218c0ad729211b14bb63c9b1a47
KIMOH3 <hamada111ggu@gmail.com>
2024-06-01 14:39:17 +0300

    Update Dev.py

--- ZelzalMusic/plugins/play/Dev.py
+++ ZelzalMusic/plugins/play/Dev.py
@@ -3,21 +3,19 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from strings.filters import command
 from ZelzalMusic import app
 import config
-import io
-import requests
 
+# تعريف الأمر وتحديد الوظيفة المرتبطة به
 @app.on_message(
     command(["المطور", "مطور"])
 )
 async def maker(client: Client, message: Message):
+    # الحصول على معلومات المستخدم
     user = await app.get_users(config.OWNER_ID)
     
-    if user.photo:
-        # استخدام رابط الصورة المباشرة من ملف الشخصي
-        photo_url = user.photo.big_file_id
-    else:
-        photo_url = "https://telegra.ph/file/dbacb042fe3d9276c687e.jpg"
-
+    # تحديد الصورة المستخدمة، أو استخدام صورة بديلة إذا لم تتوفر صورة
+    photo_url = user.photo.big_file_id if user.photo else "https://telegra.ph/file/dbacb042fe3d9276c687e.jpg"
+    
+    # إنشاء الرد المتكامل مع الصورة والنص والزر الخاص بمطور البوت
     await app.send_photo(
         chat_id=message.chat.id,
         photo=photo_url,
