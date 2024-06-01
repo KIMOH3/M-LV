a23afbd06dd5e6ab673502b2ce6af40e9928eb86
KIMOH3 <hamada111ggu@gmail.com>
2024-06-01 13:47:02 +0300

    Update Dev.py

--- ZelzalMusic/plugins/play/Dev.py
+++ ZelzalMusic/plugins/play/Dev.py
@@ -3,6 +3,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from strings.filters import command
 from ZelzalMusic import app
 import config
+import io
 
 @app.on_message(
     command(["المطور", "مطور"])
@@ -11,12 +12,15 @@ async def maker(client: Client, message: Message):
     user = await app.get_users(config.OWNER_ID)
     if user.photo:
         photo_url = user.photo.big_file_id
+        # تحميل الصورة من URL
+        photo_data = io.BytesIO(await app.download_media(photo_url))
     else:
-        photo_url = "https://example.com/default_profile_photo.jpg"
+        # في حالة عدم وجود صورة ملف شخصي، يمكنك استخدام صورة افتراضية أو رابط صورة أخرى
+        photo_data = "https://telegra.ph/file/dbacb042fe3d9276c687e.jpg"
 
     await app.send_photo(
         chat_id=message.chat.id,
-        photo=photo_url,
+        photo=photo_data,
         caption="~ 𝐃𝐞𝐯 𝐁𝐨𝐓",
         reply_markup=InlineKeyboardMarkup(
             [
